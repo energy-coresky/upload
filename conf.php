723d8866c5705dfbe1ec342fbd3dfb26d0ec04af
Energy <71418770+energy-coresky@users.noreply.github.com>
2023-02-15 16:02:01 +0200

    conf.php schema changed

--- conf.php
+++ conf.php
@@ -1,7 +1,16 @@
 <?php
 
-$plans = [
+return [
     'view' => ['path' => __DIR__ . '/mvc'],
-    'app' => ['type' => 'prod'],
     'cfg' => ['path' => __DIR__],
+    'app' => [
+        'type' => 'prod',
+        'options' => 'connection 
+table file
+dir var/upload
+rewrite upload
+scheme 0
+use_crop 1
+crop_sizes 200 x 200,200 x 500',
+    ],
 ];
